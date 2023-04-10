dec4defe90d0e98acf0d3fc51b649517b7e8e936
arash forghanbin <arashforghanbin@gmail.com>
2023-04-10 11:13:05 +0330

    added some functions

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,17 +7,62 @@ import { useEffect, useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [cam,setCam] = useState("")
+  const [cam, setCam] = useState("");
+  const [code, setCode] = useState("");
   const codeReader = new BrowserQRCodeReader();
   useEffect(() => {
     const detectInputDevices = async () => {
       const videoInputDevices = await BrowserCodeReader.listVideoInputDevices();
-      console.log(videoInputDevices)
-      const selectedDevice  = videoInputDevices[0].deviceId
-      setCam(selectedDevice)
+      console.log(videoInputDevices);
+      const selectedDevice = videoInputDevices[0]?.deviceId;
+      setCam(selectedDevice);
     };
     detectInputDevices();
   }, []);
+
+  function decodeContinuously(cam) {
+    codeReader.decodeFromInputVideoDeviceContinuously(
+      cam,
+      "cam",
+      (result, err) => {
+        if (result) {
+          // properly decoded qr code
+          console.log("Found QR code!", result);
+          setCode(result.text);
+        }
+
+        if (err) {
+          setCode("");
+
+          // As long as this error belongs into one of the following categories
+          // the code reader is going to continue as excepted. Any other error
+          // will stop the decoding loop.
+          //
+          // Excepted Exceptions:
+          //
+          //  - NotFoundException
+          //  - ChecksumException
+          //  - FormatException
+
+          if (err instanceof NotFoundException) {
+            console.log("No QR code found.");
+          }
+
+          if (err instanceof ChecksumException) {
+            console.log("A code was found, but it's read value was not valid.");
+          }
+
+          if (err instanceof FormatException) {
+            console.log("A code was found, but it was in a invalid format.");
+          }
+        }
+      }
+    );
+  }
+
+  useEffect(() => {
+    decodeContinuously(cam);
+  }, [cam]);
   return (
     <>
       <Head>
@@ -27,7 +72,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <video id={cam}></video>
+        <video id={cam} width={250} height={250}></video>
+        <p>Result:{code}</p>
       </main>
     </>
   );
